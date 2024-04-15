3809386c121af108679ec085adb3defbe01a58f6
resetmerlin <resetmerlin@gmail.com>
2024-04-15 09:47:06 +0900

    코드스피츠 객체 지향 1회차 공부 완료

--- 1회차/index.js
+++ 1회차/index.js
@@ -1,21 +1,46 @@
-const EssentialObject = class {
-  #name = "";
-  #screen = null;
-  // hide state 객체 지향에서는 객체를 사용한다. 객체를 사용한다는건 객체를 노출하지 않는 것이다.
-  // 객체를 노출하면 객체가 값 컨텍스트로 인해 객체 지향이 아니게 된다.
-
-  constructor(name) {
-    this.#name = name;
+const Worker = class {
+  run() {
+    console.log("working");
   }
-
-  camouflage() {
-    this.#screen = (Math.random() * 10).toString(16).replace(".", "");
-  }
-
-  // enscapsulation: 캡슐화는 매소드에 해당하는 말이다. 바깥 쪽에서는 name으로 인식한다. 외부에서 메소드 내 겍체를 모른다.
-  get name() {
-    return this.#screen || this.#name;
+  print() {
+    this.run();
   }
 };
 
-// 변화에 대한 격리가 매우 중요하다
+const HardWorker = class extends Worker {
+  run() {
+    console.log("hardWorking");
+  }
+};
+
+const Manager = class {
+  #workers;
+  constructor(...workers) {
+    if (workers.every((w) => w instanceof Worker)) this.#workers = workers;
+    else throw "invalid workders";
+  }
+  doWork() {
+    this.#workers.forEach((w) => w.run());
+  }
+};
+
+// 제어 역전
+
+const Renderer = class {
+  #view = null;
+  #base = null;
+  constructor(baseElemet) {
+    this.#base = baseElemet;
+  }
+
+  set view(v) {
+    if (v instanceof View) this.#view = v;
+    else throw `invalid view: ${v}`;
+  }
+
+  render(data) {
+    const base = this.#base,
+      view = this.#view;
+    if (!base || !view) throw "no base or view";
+  }
+};
